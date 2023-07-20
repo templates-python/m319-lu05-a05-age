aa0ce1be683a4c0307723234e021bc53102b589b
Marcel Suter <marcel.suter@bzz.ch>
2023-07-20 17:22:37 +0200

    added solution

--- age.py
+++ age.py
@@ -1,6 +1,9 @@
 def main():
-    # Write your program here and remove the line "pass"
-    pass
+    age = int(input('How old are you?\n'))
+    if age >= 0 and age <= 120:
+        print('OK')
+    else:
+        print('Impossible!')
 
 
 if __name__ == '__main__':
